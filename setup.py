2c201d11f988fb741ac74cc91e17520e217eb2f8
tarttelin <tarttelin@d15e52a2-e9b7-4433-8eb8-d85200345e01>
2010-03-22 16:15:15 +0000

    Increment the version for the next update to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='xml_models',
-      version='0.3',
+      version='0.4',
       description='XML backed models queried from external REST apis',
       author='Chris Tarttelin and Cam McHugh',
       author_email='ctarttelin@point2.com',
