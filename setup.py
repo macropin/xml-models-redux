14cb1fc6055279ccb0d2a8eda470c37cf7af62e9
jtownley <jtownley@d15e52a2-e9b7-4433-8eb8-d85200345e01>
2011-03-24 20:58:51 +0000

    Added modules to setup.py and up'd version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='xml_models',
-      version='0.5.2',
-      description='XML backed models queried from external REST apis',
+      version='0.6.0',
+      description='JSON/XML backed models queried from external REST apis',
       author='Chris Tarttelin and Cam McHugh',
       author_email='chris@pyruby.com',
       url='http://djangorestmodel.sourceforge.net/',
-      packages=['rest_client', 'xml_models'],
+      packages=['rest_client', 'xml_models', 'json_models','common_models'],
       install_requires=['mock', 'py-dom-xpath']
      )
